ec83eaa7e7baedd1fb6954bdb03a85f341c9a5bb
Isabel S <42853091+IsaGoodFriend@users.noreply.github.com>
2021-11-28 01:09:03 -0700

    Cleanup Code a bit

--- PixtroEditor/ColorSchemes.cs
+++ PixtroEditor/ColorSchemes.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Monocle;
 
-namespace Pixtro.Editor {
+namespace Pixtro {
 	public struct ColorSchemes {
 		public static ColorSchemes CurrentScheme { get; set; } = DarkTheme;
 
